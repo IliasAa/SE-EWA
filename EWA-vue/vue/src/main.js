e11c8752ab9d72fc10a435ab61e6be81a9dd8cb8
damien <damien.el.mesbahi@hva.nl>
2023-02-22 16:01:47 +0100

    Onnodige dingen verwijderd en files aangepast om WEB methodes toe te passen.

--- EWA-vue/vue/src/main.js
+++ EWA-vue/vue/src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import {router} from '@/router'
 
-import './assets/main.css'
 
-const app = createApp(App)
+createApp(App).use(router).mount('#app')
 
-app.use(router)
-
-app.mount('#app')
