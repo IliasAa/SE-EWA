baaf7870e9413417f44a73896c09388072b19726
andrebosker <andre.bosker@hva.nl>
2023-03-15 15:35:43 +0100

    rulepage

--- EWA-vue/src/components/rulePage.vue
+++ EWA-vue/src/components/rulePage.vue
@@ -1,4 +1,5 @@
 <template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <body>
   <nav class="navbar">
     <a class="navbar-brand" href="#">
@@ -195,6 +196,5 @@ button {
   border-bottom-color: purple;
 }
 body {
-  background-color: blueviolet;
-}
+background-image: url("@/assets/Ludo background.png")}
 </style>
