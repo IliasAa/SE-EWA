29efd50bd9819fdf0139b3c406a95c4b9f89e1cb
ibrahim <Ibrahim.ghzawi@hva.nl>
2023-03-18 21:08:04 +0100

    HomePage toegevoegd

--- EWA-vue/src/router/index.js
+++ EWA-vue/src/router/index.js
@@ -4,12 +4,14 @@ import Ludoscreen from "@/components/Ludoscreen.vue";
 import Dashboard from "@/components/Dashboard.vue";
 import rulePage from "@/components/rulePage.vue";
 import RegisterScreen from "@/components/RegisterScreen.vue";
+import homePage from "@/components/homePage.vue";
 
 const routes = [
+    {path: '/homePage', component: homePage},
     {path: '/Loginpage', component: LoginScreen},
     {path: '/gamepage', component: Ludoscreen},
     {path: '/Dashboard', component: Dashboard},
-    {path: '/', redirect: '/Loginpage'},
+    {path: '/', redirect: '/homePage'},
     {path: '/rulepage', component: rulePage},
     {path: '/RegisterScreen', component: RegisterScreen}
 ];
