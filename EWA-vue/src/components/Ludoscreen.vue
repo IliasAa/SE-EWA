052f2d1a268584d6d1789b4044aba9b83b7fc6d7
Ilias Aabi <ilias.aabi@hva.nl>
2023-03-20 04:31:39 +0100

    Path is moving good for preview

--- EWA-vue/src/components/Ludoscreen.vue
+++ EWA-vue/src/components/Ludoscreen.vue
@@ -48,19 +48,16 @@
         <div class="cells" style="top: 40%;left:26.64%;"></div>
         <div class="cells" style="top: 40%;left:33.3%;"></div>
 
-        <div class="cells" style="top: 0;left:40%;"></div>
-        <div class="cells" style="top: 6.66%;left:40%;"></div>
-        <div class="cells safe" style="top: 13.32%;left:40%;"></div>
-        <div class="cells" style="top: 19.98%;left:40%;"></div>
-        <div class="cells" style="top: 26.64%;left:40%;"></div>
         <div class="cells" style="top: 33.3%;left:40%;"></div>
+        <div class="cells" style="top: 26.64%;left:40%;"></div>
+        <div class="cells" style="top: 19.98%;left:40%;"></div>
+        <div class="cells safe" style="top: 13.32%;left:40%;"></div>
+        <div class="cells" style="top: 6.66%;left:40%;"></div>
+        <div class="cells" style="top: 0;left:40%;"></div>
+
 
         <div class="cells" style="top: 0;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 6.66%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 13.32%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 19.98%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 26.64%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 33.3%;left:46.66%;"></div>
+
 
         <div class="cells" style="top: 0;left:53.32%;"></div>
         <div class="cells y-start" style="top: 6.66%;left:53.32%;"></div>
@@ -75,34 +72,24 @@
         <div class="cells safe" style="top: 40%;right:13.32%;"></div>
         <div class="cells" style="top: 40%;right:6.66%;"></div>
         <div class="cells" style="top: 40%;right:0;"></div>
-
-        <div class="cells blue" style="top: 46.66%; right: 33.3%"></div>
-        <div class="cells blue" style="top: 46.66%;right:26.64%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:19.98%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:13.32%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:6.66%;"></div>
         <div class="cells" style="top: 46.66%;right:0;"></div>
 
-        <div class="cells" style="top: 53.32%; right: 33.3%"></div>
-        <div class="cells" style="top: 53.32%;right:26.64%;"></div>
-        <div class="cells" style="top: 53.32%;right:19.98%;"></div>
-        <div class="cells" style="top: 53.32%;right:13.32%;"></div>
+        <div class="cells" style="top: 53.32%;right: 0;"></div>
         <div class="cells b-start" style="top: 53.32%;right:6.66%;"></div>
-        <div class="cells" style="top: 53.32%;right:0;"></div>
-
-        <div class="cells" style="bottom: 0;left:53.32%;"></div>
-        <div class="cells" style="bottom: 6.66%;left:53.32%;"></div>
-        <div class="cells safe" style="bottom: 13.32%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 19.98%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 26.64%;left:53.32%;"></div>
+        <div class="cells" style="top: 53.32%;right:13.32%;"></div>
+        <div class="cells" style="top: 53.32%;right:19.98%;"></div>
+        <div class="cells" style="top: 53.32%;right:26.64%;"></div>
+        <div class="cells" style="top: 53.32%; right: 33.3%"></div>
+        
         <div class="cells" style="bottom: 33.3%;left:53.32%;"></div>
+        <div class="cells" style="bottom: 26.64%;left:53.32%;"></div>
+        <div class="cells" style="bottom: 19.98%;left:53.32%;"></div>
+        <div class="cells safe" style="bottom: 13.32%;left:53.32%;"></div>
+        <div class="cells" style="bottom: 6.66%;left:53.32%;"></div>
+        <div class="cells" style="bottom: 0;left:53.32%;"></div>
 
-        <div class="cells" style="bottom: 0;left:46.66%;"></div>
-        <div class="cells red " style="bottom: 6.66%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 13.32%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 19.98%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 26.64%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 33.3%;left:46.66%;"></div>
+
+
 
         <div class="cells" style="bottom: 0;left:40%;"></div>
         <div class="cells r-start" style="bottom: 6.66%;left:40%;"></div>
@@ -117,13 +104,32 @@
         <div class="cells safe" style="top: 53.32%;left:13.32%;"></div>
         <div class="cells" style="top: 53.32%;left:6.66%;"></div>
         <div class="cells" style="top: 53.32%;left:0;"></div>
+        <div class="cells" style="bottom: 0;left:46.66%;"></div>
+
+        <div class="cells" style="top: 46.66%;left:0;"></div>
+        <div class="cells blue" style="top: 46.66%; right: 33.3%"></div>
+        <div class="cells blue" style="top: 46.66%;right:26.64%;"></div>
+        <div class="cells blue" style="top: 46.66%;right:19.98%;"></div>
+        <div class="cells blue" style="top: 46.66%;right:13.32%;"></div>
+        <div class="cells blue" style="top: 46.66%;right:6.66%;"></div>
 
         <div class="cells green" style="top: 46.66%;left: 33.3%"></div>
         <div class="cells green" style="top: 46.66%;left:26.64%;"></div>
         <div class="cells green" style="top: 46.66%;left:19.98%;"></div>
         <div class="cells green" style="top: 46.66%;left:13.32%;"></div>
         <div class="cells green" style="top: 46.66%;left:6.66%;"></div>
-        <div class="cells" style="top: 46.66%;left:0;"></div>
+
+        <div class="cells red " style="bottom: 6.66%;left:46.66%;"></div>
+        <div class="cells red" style="bottom: 13.32%;left:46.66%;"></div>
+        <div class="cells red" style="bottom: 19.98%;left:46.66%;"></div>
+        <div class="cells red" style="bottom: 26.64%;left:46.66%;"></div>
+        <div class="cells red" style="bottom: 33.3%;left:46.66%;"></div>
+
+        <div class="cells yellow" style="top: 6.66%;left:46.66%;"></div>
+        <div class="cells yellow" style="top: 13.32%;left:46.66%;"></div>
+        <div class="cells yellow" style="top: 19.98%;left:46.66%;"></div>
+        <div class="cells yellow" style="top: 26.64%;left:46.66%;"></div>
+        <div class="cells yellow" style="top: 33.3%;left:46.66%;"></div>
       </div>
       <h2 class="dice-output">{{ diceoutput }}</h2>
       <button class="btn btn-primary" @click="ThrowDice">Gooi je dobbelsteen</button>
