f667dbccd230ff7f61e7d873aaeeda29f0031995
damien <damien.el.mesbahi@hva.nl>
2023-03-25 03:12:20 +0100

    Pawns have a set path they follow and gpawn1 test works fully.

--- EWA-vue/src/components/Ludoscreen.vue
+++ EWA-vue/src/components/Ludoscreen.vue
@@ -7,129 +7,155 @@
         <div class="house green">
 
           <div class="box">
-            <div ref="square1" class="square square-one green">
-              <div ref="pawn1" class="green-pawn1"></div>
+            <div id="100" class="square square-one green">
+              <div v-bind="this.pawns[0]" ref="gpawn1" class="green-pawn 1"></div>
             </div>
-            <div class="square square-two green"></div>
-            <div class="square square-three green"></div>
-            <div class="square square-four green"></div>
+            <div id="101" class="square square-two green">
+              <div v-bind="this.pawns[1]" class="green-pawn 2"></div>
+            </div>
+            <div id="102" class="square square-three green">
+              <div v-bind="this.pawns[2]" class="green-pawn 3"></div>
+            </div>
+            <div id="103" class="square square-four green">
+              <div v-bind="this.pawns[3]" class="green-pawn 4"></div>
+            </div>
           </div>
         </div>
         <div class="house yellow" style="right: 0">
           <div class="box">
-            <div class="square square-one yellow"></div>
-            <div class="square square-two yellow"></div>
-            <div class="square square-three yellow"></div>
-            <div class="square square-four yellow"></div>
+            <div id="200" class="square square-one yellow">
+              <div v-bind="this.pawns[4]" class="yellow-pawn 1"></div>
+            </div>
+            <div id="201" class="square square-two yellow">
+              <div v-bind="this.pawns[5]" class="yellow-pawn 2"></div>
+            </div>
+            <div id="202" class="square square-three yellow">
+              <div v-bind="this.pawns[6]" class="yellow-pawn 3"></div>
+            </div>
+            <div id="203" class="square square-four yellow">
+              <div v-bind="this.pawns[7]" class="yellow-pawn 4"></div>
+            </div>
           </div>
         </div>
         <div class="house red" style="bottom: 0">
           <div class="box">
-            <div class="square square-one red"></div>
-            <div class="square square-two red"></div>
-            <div class="square square-three red"></div>
-            <div class="square square-four red"></div>
+            <div id="300" class="square square-one red">
+              <div v-bind="this.pawns[8]" class="red-pawn 1"></div>
+            </div>
+            <div id="301" class="square square-two red">
+              <div v-bind="this.pawns[9]" class="red-pawn 2"></div>
+            </div>
+            <div id="302" class="square square-three red">
+              <div v-bind="this.pawns[10]" class="red-pawn 3"></div>
+            </div>
+            <div id="303" class="square square-four red">
+              <div v-bind="this.pawns[11]" class="red-pawn 4"></div>
+            </div>
           </div>
         </div>
         <div class="house blue" style="bottom: 0;right: 0">
           <div class="box">
-            <div class="square square-one blue"></div>
-            <div class="square square-two blue"></div>
-            <div class="square square-three blue"></div>
-            <div class="square square-four blue"></div>
+            <div id="400" class="square square-one blue">
+              <div v-bind="this.pawns[12]" class="blue-pawn 1"></div>
+            </div>
+            <div id="401" class="square square-two blue">
+              <div v-bind="this.pawns[13]" class="blue-pawn 2"></div>
+            </div>
+            <div id="402" class="square square-three blue">
+              <div v-bind="this.pawns[14]" class="blue-pawn 3"></div>
+            </div>
+            <div id="403" class="square square-four blue">
+              <div v-bind="this.pawns[15]" class="blue-pawn 4"></div>
+            </div>
           </div>
         </div>
 
         <div class="home"></div>
-        <div class="cells" style="top: 40%;"></div>
-        <div ref="start-square" class="cells g-start" style="top: 40%;left:6.66%;"></div>
-        <div ref="cell1" class="cells" style="top: 40%;left:13.32%;"></div>
-        <div ref="cell2" class="cells" style="top: 40%;left:19.98%;"></div>
-        <div class="cells" style="top: 40%;left:26.64%;"></div>
-        <div class="cells" style="top: 40%;left:33.3%;"></div>
+        <div id="51" class="cells" style="top: 40%;"></div>
+        <div id="0" class="cells g-start" style="top: 40%;left:6.66%;"></div>
+        <div id="1" class="cells" style="top: 40%;left:13.32%;"></div>
+        <div id="2" class="cells" style="top: 40%;left:19.98%;"></div>
+        <div id="3" class="cells" style="top: 40%;left:26.64%;"></div>
+        <div id="4" class="cells" style="top: 40%;left:33.3%;"></div>
 
-        <div class="cells" style="top: 33.3%;left:40%;"></div>
-        <div class="cells" style="top: 26.64%;left:40%;"></div>
-        <div class="cells" style="top: 19.98%;left:40%;"></div>
-        <div class="cells safe" style="top: 13.32%;left:40%;"></div>
-        <div class="cells" style="top: 6.66%;left:40%;"></div>
-        <div class="cells" style="top: 0;left:40%;"></div>
-
-
-        <div class="cells" style="top: 0;left:46.66%;"></div>
+        <div id="5" class="cells" style="top: 33.3%;left:40%;"></div>
+        <div id="6" class="cells" style="top: 26.64%;left:40%;"></div>
+        <div id="7" class="cells" style="top: 19.98%;left:40%;"></div>
+        <div id="8" class="cells safe" style="top: 13.32%;left:40%;"></div>
+        <div id="9" class="cells" style="top: 6.66%;left:40%;"></div>
+        <div id="10" class="cells" style="top: 0;left:40%;"></div>
 
 
-        <div class="cells" style="top: 0;left:53.32%;"></div>
-        <div class="cells y-start" style="top: 6.66%;left:53.32%;"></div>
-        <div class="cells" style="top: 13.32%;left:53.32%;"></div>
-        <div class="cells" style="top: 19.98%;left:53.32%;"></div>
-        <div class="cells" style="top: 26.64%;left:53.32%;"></div>
-        <div class="cells" style="top: 33.3%;left:53.32%;"></div>
+        <div id="11" class="cells" style="top: 0;left:46.66%;"></div>
+        <div id="12" class="cells" style="top: 0;left:53.32%;"></div>
+        <div id="13" class="cells y-start" style="top: 6.66%;left:53.32%;"></div>
+        <div id="14" class="cells" style="top: 13.32%;left:53.32%;"></div>
+        <div id="15" class="cells" style="top: 19.98%;left:53.32%;"></div>
+        <div id="16" class="cells" style="top: 26.64%;left:53.32%;"></div>
+        <div id="17" class="cells" style="top: 33.3%;left:53.32%;"></div>
 
-        <div class="cells" style="top: 40%; right: 33.3%"></div>
-        <div class="cells" style="top: 40%;right:26.64%;"></div>
-        <div class="cells" style="top: 40%;right:19.98%;"></div>
-        <div class="cells safe" style="top: 40%;right:13.32%;"></div>
-        <div class="cells" style="top: 40%;right:6.66%;"></div>
-        <div class="cells" style="top: 40%;right:0;"></div>
-        <div class="cells" style="top: 46.66%;right:0;"></div>
+        <div id="18" class="cells" style="top: 40%; right: 33.3%"></div>
+        <div id="19" class="cells" style="top: 40%;right:26.64%;"></div>
+        <div id="20" class="cells" style="top: 40%;right:19.98%;"></div>
+        <div id="21" class="cells safe" style="top: 40%;right:13.32%;"></div>
+        <div id="22" class="cells" style="top: 40%;right:6.66%;"></div>
+        <div id="23" class="cells" style="top: 40%;right:0;"></div>
+        <div id="24" class="cells" style="top: 46.66%;right:0;"></div>
 
-        <div class="cells" style="top: 53.32%;right: 0;"></div>
-        <div class="cells b-start" style="top: 53.32%;right:6.66%;"></div>
-        <div class="cells" style="top: 53.32%;right:13.32%;"></div>
-        <div class="cells" style="top: 53.32%;right:19.98%;"></div>
-        <div class="cells" style="top: 53.32%;right:26.64%;"></div>
-        <div class="cells" style="top: 53.32%; right: 33.3%"></div>
-        
-        <div class="cells" style="bottom: 33.3%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 26.64%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 19.98%;left:53.32%;"></div>
-        <div class="cells safe" style="bottom: 13.32%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 6.66%;left:53.32%;"></div>
-        <div class="cells" style="bottom: 0;left:53.32%;"></div>
+        <div id="25" class="cells" style="top: 53.32%;right: 0;"></div>
+        <div id="26" class="cells b-start" style="top: 53.32%;right:6.66%;"></div>
+        <div id="27" class="cells" style="top: 53.32%;right:13.32%;"></div>
+        <div id="28" class="cells" style="top: 53.32%;right:19.98%;"></div>
+        <div id="29" class="cells" style="top: 53.32%;right:26.64%;"></div>
+        <div id="30" class="cells" style="top: 53.32%; right: 33.3%"></div>
 
-
+        <div id="31" class="cells" style="bottom: 33.3%;left:53.32%;"></div>
+        <div id="32" class="cells" style="bottom: 26.64%;left:53.32%;"></div>
+        <div id="33" class="cells" style="bottom: 19.98%;left:53.32%;"></div>
+        <div id="34" class="cells safe" style="bottom: 13.32%;left:53.32%;"></div>
+        <div id="35" class="cells" style="bottom: 6.66%;left:53.32%;"></div>
+        <div id="36" class="cells" style="bottom: 0;left:53.32%;"></div>
 
 
-        <div class="cells" style="bottom: 0;left:40%;"></div>
-        <div class="cells r-start" style="bottom: 6.66%;left:40%;"></div>
-        <div class="cells" style="bottom: 13.32%;left:40%;"></div>
-        <div class="cells" style="bottom: 19.98%;left:40%;"></div>
-        <div class="cells" style="bottom: 26.64%;left:40%;"></div>
-        <div class="cells" style="bottom: 33.3%;left:40%;"></div>
+        <div id="37" class="cells" style="bottom: 0;left:46.66%;"></div>
+        <div id="38" class="cells" style="bottom: 0;left:40%;"></div>
+        <div id="39" class="cells r-start" style="bottom: 6.66%;left:40%;"></div>
+        <div id="40" class="cells" style="bottom: 13.32%;left:40%;"></div>
+        <div id="41" class="cells" style="bottom: 19.98%;left:40%;"></div>
+        <div id="42" class="cells" style="bottom: 26.64%;left:40%;"></div>
+        <div id="43" class="cells" style="bottom: 33.3%;left:40%;"></div>
 
-        <div class="cells" style="top: 53.32%; left: 33.3%"></div>
-        <div class="cells" style="top: 53.32%;left:26.64%;"></div>
-        <div class="cells" style="top: 53.32%;left:19.98%;"></div>
-        <div class="cells safe" style="top: 53.32%;left:13.32%;"></div>
-        <div class="cells" style="top: 53.32%;left:6.66%;"></div>
-        <div class="cells" style="top: 53.32%;left:0;"></div>
-        <div class="cells" style="bottom: 0;left:46.66%;"></div>
+        <div id="44" class="cells" style="top: 53.32%; left: 33.3%"></div>
+        <div id="45" class="cells" style="top: 53.32%;left:26.64%;"></div>
+        <div id="46" class="cells" style="top: 53.32%;left:19.98%;"></div>
+        <div id="47" class="cells safe" style="top: 53.32%;left:13.32%;"></div>
+        <div id="48" class="cells" style="top: 53.32%;left:6.66%;"></div>
+        <div id="49" class="cells" style="top: 53.32%;left:0;"></div>
+        <div id="50" class="cells" style="top: 46.66%;left:0;"></div>
 
-        <div class="cells" style="top: 46.66%;left:0;"></div>
-        <div class="cells blue" style="top: 46.66%; right: 33.3%"></div>
-        <div class="cells blue" style="top: 46.66%;right:26.64%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:19.98%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:13.32%;"></div>
-        <div class="cells blue" style="top: 46.66%;right:6.66%;"></div>
+        <div id="65" class="cells blue" style="top: 46.66%; right: 33.3%"></div>
+        <div id="64" class="cells blue" style="top: 46.66%;right:26.64%;"></div>
+        <div id="63" class="cells blue" style="top: 46.66%;right:19.98%;"></div>
+        <div id="62" class="cells blue" style="top: 46.66%;right:13.32%;"></div>
+        <div id="61" class="cells blue" style="top: 46.66%;right:6.66%;"></div>
 
-        <div class="cells green" style="top: 46.66%;left: 33.3%"></div>
-        <div class="cells green" style="top: 46.66%;left:26.64%;"></div>
-        <div class="cells green" style="top: 46.66%;left:19.98%;"></div>
-        <div class="cells green" style="top: 46.66%;left:13.32%;"></div>
-        <div class="cells green" style="top: 46.66%;left:6.66%;"></div>
+        <div id="56" class="cells green" style="top: 46.66%;left: 33.3%"></div>
+        <div id="55" class="cells green" style="top: 46.66%;left:26.64%;"></div>
+        <div id="54" class="cells green" style="top: 46.66%;left:19.98%;"></div>
+        <div id="53" class="cells green" style="top: 46.66%;left:13.32%;"></div>
+        <div id="52" class="cells green" style="top: 46.66%;left:6.66%;"></div>
 
-        <div class="cells red " style="bottom: 6.66%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 13.32%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 19.98%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 26.64%;left:46.66%;"></div>
-        <div class="cells red" style="bottom: 33.3%;left:46.66%;"></div>
+        <div id="71" class="cells red " style="bottom: 6.66%;left:46.66%;"></div>
+        <div id="72" class="cells red" style="bottom: 13.32%;left:46.66%;"></div>
+        <div id="73" class="cells red" style="bottom: 19.98%;left:46.66%;"></div>
+        <div id="74" class="cells red" style="bottom: 26.64%;left:46.66%;"></div>
+        <div id="75" class="cells red" style="bottom: 33.3%;left:46.66%;"></div>
 
-        <div class="cells yellow" style="top: 6.66%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 13.32%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 19.98%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 26.64%;left:46.66%;"></div>
-        <div class="cells yellow" style="top: 33.3%;left:46.66%;"></div>
+        <div id="81" class="cells yellow" style="top: 6.66%;left:46.66%;"></div>
+        <div id="82" class="cells yellow" style="top: 13.32%;left:46.66%;"></div>
+        <div id="83" class="cells yellow" style="top: 19.98%;left:46.66%;"></div>
+        <div id="84" class="cells yellow" style="top: 26.64%;left:46.66%;"></div>
+        <div id="85" class="cells yellow" style="top: 33.3%;left:46.66%;"></div>
       </div>
       <h2 class="dice-output">{{ diceoutput }}</h2>
       <button class="btn btn-primary" @click="ThrowDice">Gooi je dobbelsteen</button>
@@ -146,56 +172,87 @@
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import {pawn} from "@/models/pawn"
 
 export default {
   name: "LoginScreen",
   components: {NavBar},
   data() {
     return {
+      pawns: [],
       diceoutput: null,
       pawnSteps: 0,
     };
   },
 
+  created() {
+    for (let i = 1000; i < 1004; i++) {
+      this.pawns.push(pawn.createGreenpawn(i))
+    }
+    for (let i = 2000; i < 2004; i++) {
+      this.pawns.push(pawn.createYellowpawn(i))
+    }
+    for (let i = 3000; i < 3004; i++) {
+      this.pawns.push(pawn.createRedpawn(i))
+    }
+    for (let i = 4000; i < 4004; i++) {
+      this.pawns.push(pawn.createbluePawn(i))
+    }
+  },
+
   methods: {
 
-    movePawn() {
-      const pawn = this.$refs.pawn1;
-      if (this.pawnSteps === 0) {
-        const greenSquare = this.$refs.square1;
-        const startSquare = this.$refs["start-square"];
-        greenSquare.removeChild(pawn);
-        startSquare.appendChild(pawn);
-        this.pawnSteps++;
+    newPawn(result) {
+      const pawnMove = this.$refs.gpawn1;
+
+      if (this.pawns[0].onField === 1){
+        this.pawns[0].previousPosition = 100;
+        this.pawns[0].position = this.pawns[0].path[0];
+        let prevPosBox = document.getElementById(this.pawns[0].previousPosition);
+        let nextPosBox = document.getElementById(this.pawns[0].position);
+        prevPosBox.removeChild(pawnMove);
+        nextPosBox.appendChild(pawnMove);
+        this.pawns[0].onField = 2;
       } else {
-        let parent = pawn.parentElement;
-        pawn.parentElement.removeChild(pawn);
-        parent.nextElementSibling.appendChild(pawn);
-        console.log(parent)
+        this.movePawn(result)
       }
-
-
     },
+
+    movePawn(result) {
+      const pawnMove = this.$refs.gpawn1;
+      if (this.pawns[0].onField === 2) {
+        const posValue = this.pawns[0].position;
+        const currPosIndex = this.pawns[0].path.indexOf(posValue);
+        let newPosIndex = currPosIndex + result;
+        const arrLength = this.pawns[0].path.length;
+        if (newPosIndex > arrLength) {
+          newPosIndex = arrLength - 1;
+        }
+        this.pawns[0].previousPosition = this.pawns[0].position;
+
+        const newPos = this.pawns[0].path[newPosIndex];
+        this.pawns[0].position = newPos;
+
+        let prevPosBox = document.getElementById(this.pawns[0].previousPosition);
+        let nextPosBox = document.getElementById(this.pawns[0].position);
+        prevPosBox.removeChild(pawnMove);
+        nextPosBox.appendChild(pawnMove);
+      }
+    },
+
+
     ThrowDice() {
       let result = Math.floor((Math.random() * 6) + 1);
       this.diceoutput = result
-      if (result === 6 && this.pawnSteps === 0) {
-        this.AddnewToken();
-        this.movePawn();
-      } else if (this.pawnSteps !== 0) {
-        // this.movePawn();
-        for (let i = 0; i < this.diceoutput; i++) {
-          setTimeout(this.movePawn, 1000 * i);
-        }
+      if (result === 6) {
+        this.newPawn(result)
+      } else {
+        this.movePawn(result);
+      }
       }
 
     },
-
-    AddnewToken() {
-      return null;
-    }
-  },
-}
+  }
 </script>
 
 
@@ -203,12 +260,66 @@ export default {
 
 /* Variables */
 
-.green-pawn1 {
+.green-pawn {
   height: 25px;
   width: 25px;
   border-radius: 100%;
   position: absolute;
-  background-color: black;
+  background-color: green;
+
+  border-width: 2px;
+  border-style: solid;
+  border-color: black;
+
+  top: 0;
+  left: 0;
+  transition: top 1s ease, left 1s ease;
+}
+
+.red-pawn {
+  height: 25px;
+  width: 25px;
+  border-radius: 100%;
+  position: absolute;
+  background-color: red;
+
+  border-width: 2px;
+  border-style: solid;
+  border-color: black;
+
+  top: 0;
+  left: 0;
+  transition: top 1s ease, left 1s ease;
+}
+
+.yellow-pawn {
+  height: 25px;
+  width: 25px;
+  border-radius: 100%;
+  position: absolute;
+  background-color: yellow;
+
+
+  border-width: 2px;
+  border-style: solid;
+  border-color: black;
+
+  top: 0;
+  left: 0;
+  transition: top 1s ease, left 1s ease;
+}
+
+.blue-pawn {
+  height: 25px;
+  width: 25px;
+  border-radius: 100%;
+  position: absolute;
+  background-color: blue;
+
+  border-width: 2px;
+  border-style: solid;
+  border-color: black;
+
   top: 0;
   left: 0;
   transition: top 1s ease, left 1s ease;
