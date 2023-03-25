64bf0ab56d1db3df68de7543c918d1fd29935390
ibrahim <Ibrahim.ghzawi@hva.nl>
2023-03-25 15:25:00 +0100

    Backend toegevoegd

---
+++ EWA-server/src/main/java/com/example/ewaserver/EwaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ewaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwaServerApplication.class, args);
+	}
+
+}
---
+++ EWA-server/src/test/java/com/example/ewaserver/EwaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
