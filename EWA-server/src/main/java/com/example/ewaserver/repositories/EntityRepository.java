51a002169db3f7bc808f9cc3811055ea5ededddc
ibrahim <Ibrahim.ghzawi@hva.nl>
2023-03-27 03:04:36 +0200

    updated the entityRepository with implementations of all CRUD methods

--- EWA-server/src/main/java/com/example/ewaserver/repositories/EntityRepository.java
+++ EWA-server/src/main/java/com/example/ewaserver/repositories/EntityRepository.java
@@ -1,5 +1,17 @@
 package com.example.ewaserver.repositories;
 
+import java.util.List;
+
 public interface EntityRepository<E> {
+
+    List<E> findAll();
+
+    E findById(int id);
+
+    E Save(E entity);
+
+    E update(E entity, int id);
+
+    E deleteById(int id);
 
 }
