7f1baa4137fa7e0afc5552151ad3bf1a9c2226bc
andrebosker <andre.bosker@hva.nl>
2023-04-30 13:59:30 +0200

    LobbyController en repo

--- EWA-server/src/main/java/com/example/ewaserver/rest/LobbyController.java
+++ EWA-server/src/main/java/com/example/ewaserver/rest/LobbyController.java
@@ -1,12 +1,16 @@
 package com.example.ewaserver.rest;
 
 
+import com.example.ewaserver.exceptions.ResourceNotFoundException;
 import com.example.ewaserver.models.Lobby;
+import com.example.ewaserver.models.User;
 import com.example.ewaserver.repositories.LobbyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,9 +25,31 @@ public class LobbyController {
         return lobbyRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Lobby getLobbyByCode(@PathVariable int id){
+        Lobby lobbyCode = lobbyRepository.findById(id);
+        if (lobbyCode == null) {
+            throw new ResourceNotFoundException("lobby not found with id: " + id);
+        }
+        return lobbyCode;
+    }
+
     @PostMapping("/onlineGame")
     public ResponseEntity<Object> CreateNewLobby(@RequestBody Lobby lobby) {
 
-        return null;
+        Lobby saveLobby = lobbyRepository.Save(lobby);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().
+                path("/{id}").buildAndExpand(saveLobby.getLobbyId()).toUri();
+        return ResponseEntity.created(location).body(saveLobby);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public Lobby deleteOwnUser(@PathVariable() int id) {
+
+        Lobby lobby = this.lobbyRepository.deleteById(id);
+        if (lobby == null) {
+            throw new ResourceNotFoundException("Cannot delete an lobby with id=" + id);
+        }
+        return lobby;
     }
 }
