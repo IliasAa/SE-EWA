364e93627bd46f0b1c8b6c6f9ff06af4912497ed
andrebosker <andre.bosker@hva.nl>
2023-05-30 14:39:34 +0200

    joinen van een game met het kiezen van een kleur

--- EWA-vue/src/components/lobby/JoinGame.vue
+++ EWA-vue/src/components/lobby/JoinGame.vue
@@ -9,7 +9,31 @@
             type="text"
             v-model="join_code"
         ></v-text-field>
-        <button class="btn btn-info" @click="Joingame(this.join_code)">Join a lobby using a code</button>
+        <button class="btn btn-primary btn-lg" @click="showPopup1 = !showPopup1">Join a lobby using a code</button>
+        <popup class="popup" v-if="showPopup1">
+          <div class="lobbyDetail" :style="{display: showPopup1 ? 'block' : 'none'}">
+            <div class="lobbyContent">
+              <span @click="showPopup1 = !showPopup1" class="close">&times;</span>
+              <div class="playerColor">
+                <p>Choose a starting color:</p>
+                <button class="playerColorButton" :class="{ active: selectedColor === 'red'}"
+                        id="playerRed" @click="colorChoosing('red')">Red
+                </button>
+                <button class="playerColorButton" :class="{ active: selectedColor === 'blue'}"
+                        id="playerBlue" @click="colorChoosing('blue')">Blue
+                </button>
+                <button class="playerColorButton" :class="{ active: selectedColor === 'yellow'}"
+                        id="playerYellow" @click="colorChoosing('yellow')">Yellow
+                </button>
+                <button class="playerColorButton" :class="{ active: selectedColor === 'green'}"
+                        id="playerGreen" @click="colorChoosing('green')">Green
+                </button>
+                <button class="btn btn-primary btn-sm play" @click="Joingame(join_code)">&#9658;</button>
+              </div>
+            </div>
+          </div>
+        </popup>
+<!--        <button class="btn btn-info" @click="Joingame(this.join_code)">Join a lobby using a code</button>-->
         <table class="table">
           <thead>
           <tr>
@@ -26,7 +50,30 @@
             <td>{{game.creatorNames}}</td>
             <td>{{ game.player_size + "/" + game.max_allowed_Players }}</td>
             <td>
-              <button class="btn btn-primary btn-sm play" @click="Joingame(game.join_code)">&#9658;</button>
+              <button class="btn btn-primary btn-lg" @click="showPopup2 = !showPopup2">&#9658;</button>
+              <popup class="popup" v-if="showPopup2">
+                <div class="lobbyDetail" :style="{display: showPopup2 ? 'block' : 'none'}">
+                  <div class="lobbyContent">
+                    <span @click="showPopup2 = !showPopup2" class="close">&times;</span>
+                    <div class="playerColor">
+                      <p>Choose a starting color:</p>
+                      <button class="playerColorButton" :class="{ active: selectedColor === 'red'}"
+                              id="playerRed" @click="colorChoosing('red')">Red
+                      </button>
+                      <button class="playerColorButton" :class="{ active: selectedColor === 'blue'}"
+                              id="playerBlue" @click="colorChoosing('blue')">Blue
+                      </button>
+                      <button class="playerColorButton" :class="{ active: selectedColor === 'yellow'}"
+                              id="playerYellow" @click="colorChoosing('yellow')">Yellow
+                      </button>
+                      <button class="playerColorButton" :class="{ active: selectedColor === 'green'}"
+                              id="playerGreen" @click="colorChoosing('green')">Green
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <button class="btn btn-primary btn-sm play" @click="Joingame(game.join_code)">&#9658;</button>
+              </popup>
             </td>
           </tr>
           </tbody>
@@ -59,10 +106,14 @@ export default {
       mygames: [],
       games: [],
       join_code: "",
+      selectedColor: null,
 
       users: [],
       selectedColorsinLobby: [],
       joiningWithColor: "",
+      showPopup1: false,
+      showPopup2: false,
+
     }
   },
   async created() {
@@ -100,35 +151,23 @@ export default {
 
   methods: {
 
+    colorChoosing(color) {
+      this.selectedColor = color;
+    },
     //Async method to join a game using a join code of that lobby
     async Joingame(join_code) {
       //saves the response and send it to the User_has_lobby.
 
       //dummy data to test combination between user and lobby
       //this has to be removed if the selectColor pop-up is implemented
-      const selectedcolor = "green";
+      const selectedcolor = this.selectedColor;
 
       const createdLobby = await this.lobbyService.asyncFindByjoincode(join_code);
 
       await this.lobbyService.combineUserWithLobby(this.userId, createdLobby[0].idLobby, selectedcolor);
-
       //Push router to lobby with join code so it will see it in the params
       this.$router.push("/lobby/" + createdLobby[0].join_code)
     },
-
-
-    //async method to show all the colors that are already selected in the selected lobby.
-    //this method will be used on the detail
-    async SelectedColors(join_code) {
-      const createdLobby = await this.lobbyService.asyncFindByjoincode(join_code);
-      this.users = await this.lobbyService.asyncFindAllConnectedToLobby(createdLobby[0].idLobby);
-
-      for (let i = 0; i < this.users.length; i++) {
-        const returnStatement =
-            await this.lobbyService.asyncFindColorConnectedToUser(createdLobby, this.users[i].userId);
-        this.selectedColorsinLobby.push(returnStatement[0]);
-      }
-    }
 
   }
 }
