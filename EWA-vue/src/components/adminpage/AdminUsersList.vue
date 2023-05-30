18e69250934e3fb7e8288d88aca8f232b576b75e
ibrahim <Ibrahim.ghzawi@hva.nl>
2023-05-30 04:12:46 +0200

    Admin design final done

--- EWA-vue/src/components/adminpage/AdminUsersList.vue
+++ EWA-vue/src/components/adminpage/AdminUsersList.vue
@@ -1,41 +1,67 @@
 <template>
  <NavBar></NavBar>
 
-    <div class="screen">
-
-        <div class="user-list-card">
-            <h1 class="user-list-title">User List</h1>
-            <p class="user-list-subtitle">Edit or delete users</p>
-            <div class="user-table">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Email</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="user in users" :key="user">
-                        <td>{{ user.username }}</td>
-                        <td>{{ user.firstname }}</td>
-                        <td>{{ user.lastname }}</td>
-                        <td>{{ user.email }}</td>
-                        <td>
-                            <v-btn>Edit</v-btn>
-                            <v-btn @click="deleteUser(user.userId)">Delete</v-btn>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+    <div>
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"/>
+        <hr/>
+        <div class="container bootstrap snippets bootdey">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="main-box no-header clearfix">
+                        <div class="main-box-body clearfix">
+                            <div class="table-responsive">
+                                <table class="table user-list">
+                                    <thead>
+                                    <tr>
+                                        <th><span>Username</span></th>
+                                        <th><span>Firstname</span></th>
+                                        <th><span>Lastname</span></th>
+                                        <th><span>Email</span></th>
+                                        <th>&nbsp;</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="user in users" :key="user">
+                                        <td>
+                                            <img src="https://bootdey.com/img/Content/user_1.jpg" alt=""/>
+                                            <a href="#/" class="user-link">{{user.username}}</a>
+                                            <span class="user-subhead">{{user.role}}</span>
+                                        </td>
+                                        <td>{{user.firstname}}</td>
+                                        <td>{{user.lastname}}</td>
+                                        <td>{{user.email}}</td>
+                                        <td>
+                                            <a href="#/" class="table-link text-info">
+                                                <span class="fa-stack">
+                                                    <i class="fa fa-square fa-stack-2x"></i>
+                                                    <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                                </span>
+                                            </a>
+                                            <a href="#/" class="table-link danger">
+                                                <span class="fa-stack">
+                                                    <i class="fa fa-square fa-stack-2x"></i>
+                                                    <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                                </span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">New User</h5>
+                            <button class="btn btn-success">Add User</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
-
-
 
 
     <div class="background">
@@ -77,7 +103,7 @@ export default {
             toast.error("Account couldn't be deleted, please check the console.");
           }
         }
-      }
+      },
   }
 }
 
@@ -100,66 +126,73 @@ export default {
     background-color: rgba(5, 11, 98, 1);
 }
 
-.screen {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+body{
+    background:#eee;
+}
+.main-box.no-header {
+    padding-top: 20px;
+}
+.main-box {
+    background: #FFFFFF;
+    -webkit-box-shadow: 1px 1px 2px 0 #CCCCCC;
+    -moz-box-shadow: 1px 1px 2px 0 #CCCCCC;
+    -o-box-shadow: 1px 1px 2px 0 #CCCCCC;
+    -ms-box-shadow: 1px 1px 2px 0 #CCCCCC;
+    box-shadow: 1px 1px 2px 0 #CCCCCC;
+    margin-bottom: 16px;
+    -webikt-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+}
+.table a.table-link.danger {
+    color: #e74c3c;
 }
 
-.user-list-card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background: white;
-    width: 80%;
-    padding: 20px;
-    margin-top: 50px;
+.label {
+    border-radius: 3px;
+    font-size: 0.875em;
+    font-weight: 600;
+}
+.user-list tbody td .user-subhead {
+    font-size: 0.875em;
+    font-style: italic;
+}
+.user-list tbody td .user-link {
+    display: block;
+    font-size: 1.25em;
+    padding-top: 3px;
+    margin-left: 60px;
+}
+a {
+    color: #3498db;
+    outline: none!important;
+}
+.user-list tbody td>img {
+    position: relative;
+    max-width: 50px;
+    float: left;
+    margin-right: 15px;
 }
 
-.user-list-title {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 10px;
+.table thead tr th {
+    text-transform: uppercase;
+    font-size: 0.875em;
 }
-
-.user-list-subtitle {
-    font-size: 16px;
-    margin-bottom: 20px;
+.table thead tr th {
+    border-bottom: 2px solid #e7ebee;
 }
-
-.user-list-card {
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
-    padding: 1rem;
-    margin: 50px auto 0;
-    max-width: 600px;
-    text-align: center;
+.table tbody tr td:first-child {
+    font-size: 1.125em;
+    font-weight: 300;
 }
-
-.user-table {
-    overflow-x: auto;
+.table tbody tr td {
+    font-size: 0.875em;
+    vertical-align: middle;
+    border-top: 1px solid #e7ebee;
+    padding: 12px 8px;
 }
-
-table {
-    border-collapse: collapse;
-    width: 100%;
-    margin-top: 1rem;
-}
-
-th, td {
-    padding: 0.75rem;
-    text-align: left;
-}
-
-th {
-    font-weight: bold;
-    background-color: #f7f7f7;
-}
-
-td button {
-    margin-right: 0.5rem;
+a:hover{
+    text-decoration:none;
 }
 
 
