7ca35ef3a7756f899914cc4cd664e12df8b96960
Elvis Osei Aborah <elvis.osei.aborah@hva.nl>
2023-06-05 01:45:59 +0200

    final deployment setup (before build)

--- EWA-vue/vue.config.js
+++ EWA-vue/vue.config.js
@@ -1,4 +1,10 @@
+
 const { defineConfig } = require('@vue/cli-service')
+const dotenv = require('dotenv');
+
 module.exports = defineConfig({
+
+  dotenv.config();
   transpileDependencies: true
+
 })
