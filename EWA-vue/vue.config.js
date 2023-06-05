00677fb511f7174371bdf15b8e05be597cc220b5
Elvis Osei Aborah <elvis.osei.aborah@hva.nl>
2023-06-05 02:38:10 +0200

    final rollback fix

--- EWA-vue/vue.config.js
+++ EWA-vue/vue.config.js
@@ -1,10 +1,8 @@
 
 const { defineConfig } = require('@vue/cli-service')
-const dotenv = require('dotenv');
 
 module.exports = defineConfig({
 
-  dotenv.config();
   transpileDependencies: true
 
 })
