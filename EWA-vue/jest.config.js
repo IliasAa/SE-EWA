5e8a4acbcdc46a5c02de7fd028c349b85a756092
Elvis Osei Aborah <elvis.osei.aborah@hva.nl>
2023-07-02 22:50:44 +0200

    jest config change test

--- EWA-vue/jest.config.js
+++ EWA-vue/jest.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    transformIgnorePatterns: [
-        "/node_modules/(?!bootstrap)/"  // Ignore transformation for all node_modules except bootstrap
+    // transformIgnorePatterns: [
+    //     "/node_modules/(?!bootstrap)/"  // Ignore transformation for all node_modules except bootstrap
+    // ],
+
+    "setupFilesAfterEnv": [
+        "jest-expect-message"
     ],
+    "testMatch": [
+        "**/src/**/*.spec.js"
+    ],
+    "moduleNameMapper": {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "/images/(.*)$": "<rootDir>/public/images/$1",
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    },
+    "transform": {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.vue$": "@vue/vue3-jest",
+        "^.+\\.(jpg|jpeg|png|gif|svg|mp3|mp4|mpeg)$": "<rootDir>/jest-media-transformer.js"
+    }
 };
